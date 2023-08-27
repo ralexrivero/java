18a1dc4b2b0160d1ac7bdc08059d2be05ed5b4e0
ralexrivero <ralexrivero@gmail.com>
2023-08-27 01:22:27 -0300

    user input

---
+++ especialista_desarrollo_java/capitulo-1/src/SegundoPrograma.java
@@ -0,0 +1,11 @@
+public class SegundoPrograma {
+    public static void main(String[] args) {
+        int variable1 = 5;
+        int variable2 = 10;
+        if (variable1 > variable2) {
+            System.out.println("variable1 es mayor que variable2");
+        } else {
+            System.out.println("variable2 es mayor que variable1");
+        }
+    }
+}
