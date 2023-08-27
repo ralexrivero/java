d69795ae7cc7fd9f24987948f4f852802ae17a42
ralexrivero <ralexrivero@gmail.com>
2023-08-27 00:12:51 -0300

    hello world

---
+++ especialista_desarrollo_java/capitulo-0/HolaMundo.java
@@ -0,0 +1,13 @@
+/*
+ * primer programa en Java
+ *
+ */
+
+public class HolaMundo {
+	// un programa Java necesita un metodo main() por el que comenzar
+
+	public static void main(String args[]) {
+		System.out.println("Hola mundo");
+	}
+}
+
